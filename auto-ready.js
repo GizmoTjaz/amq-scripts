099445fbdca5e11b0c8cfafee7070233a65a4e15
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-02-07 22:38:09 +0100

    auto-ready script

---
+++ auto-ready.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name        AMQ Auto-ready
+// @namespace   AMQ Scripts
+// @match       https://animemusicquiz.com/*
+// @grant       none
+// @version     1.0
+// @author      GizmoTjaz
+// @require     https://raw.githubusercontent.com/GizmoTjaz/amq-scripts/master/utils/script-info.js
+// ==/UserScript==
+
+if (document.getElementById("startPage"))
+	return;
+
+function setReady () {
+
+	if (quiz.gameMode === "Ranked" || quiz.isHost || !lobby.inLobby || lobby.isSpectator)
+		return;
+
+	if (!lobby.isReady) {
+		lobby.fireMainButtonEvent();
+	}
+}
+
+AMQ_UTILS.onViewChange = () => {
+	console.log("view change");
+	if (viewChanger.currentView === "lobby") {
+		AMQ_UTILS.randomDelay(setReady);
+	}
+}
+
+new Listener("Room Settings Changed", () => {
+	AMQ_UTILS.randomDelay(setReady);
+}).bindListener();
+
+new Listener("Spectator Change To Player", () => {
+	AMQ_UTILS.randomDelay(setReady);
+}).bindListener();
+
+new Listener("Host Promotion", () => {
+	AMQ_UTILS.randomDelay(setReady);
+}).bindListener();
+
+AMQ_addScriptData({
+	name: "Auto Ready",
+	author: "Tjizz",
+	description: `
+		<p>Automatically sets your state to ready in non-ranked games.</p>
+	`
+});
