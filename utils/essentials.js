896a5561874a7dcac141df82d0f6856b5db1d714
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-02-08 18:09:12 +0100

    improve game load checker

--- utils/essentials.js
+++ utils/essentials.js
@@ -38,7 +38,7 @@ AMQ_UTILS.onGameLoad(() => {
 });
 
 const gameLoadChecker = setInterval(() => {
-	if (window.viewChanger && viewChanger?.currentView !== undefined) {
+	if (window.quiz && window.lobby && window.viewChanger?.currentView !== undefined) {
 		clearInterval(gameLoadChecker);
 		AMQ_UTILS.__gameLoadListeners.forEach(callback => callback.apply());
 	}
