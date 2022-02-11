8d772609b2aa1d8ca0c81957c8f1e59e8518dafe
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-02-12 00:39:43 +0100

    only set up AMQ_UTILS once and delay onGameReady

--- utils/essentials.js
+++ utils/essentials.js
@@ -1,48 +1,54 @@
-const AMQ_UTILS = {
+// Only set up once
+if (!window.AMQ_UTILS) {
+	
+	window.AMQ_UTILS = {
 
-	__viewChangeListeners: [],
-	__gameLoadListeners: [],
-
-	// Functions
-	getRandomNumber: function (max) {
-		return Math.floor(Math.random() * max);
-	},
-	randomDelay: function (callback) {
-		setTimeout(callback, this.getRandomNumber(400));
-	},
-	submitAnswer: function () {
-		quiz.answerInput.$input[0].dispatchEvent(new KeyboardEvent("keypress", {
-			key: "Enter",
-			code: "Enter",
-			which: 13
-		}));
-	},
-
-	// Listeners
-	onViewChange: function (callback) {
-		this.__viewChangeListeners.push(callback);
-	},
-	onGameLoad: function (callback) {
-		this.__gameLoadListeners.push(callback);
-	}
-
-};
-
-ViewChanger.prototype.changeView = (function () {
-
-	const old = ViewChanger.prototype.changeView;
-
-	return function () {
-		
-		old.apply(this, arguments);
-
-		AMQ_UTILS.__viewChangeListeners.forEach(callback => callback.apply());
+		__viewChangeListeners: [],
+		__gameLoadListeners: [],
+	
+		// Functions
+		getRandomNumber: function (max) {
+			return Math.floor(Math.random() * max);
+		},
+		randomDelay: function (callback) {
+			setTimeout(callback, this.getRandomNumber(400));
+		},
+		submitAnswer: function () {
+			quiz.answerInput.$input[0].dispatchEvent(new KeyboardEvent("keypress", {
+				key: "Enter",
+				code: "Enter",
+				which: 13
+			}));
+		},
+	
+		// Listeners
+		onViewChange: function (callback) {
+			this.__viewChangeListeners.push(callback);
+		},
+		onGameLoad: function (callback) {
+			this.__gameLoadListeners.push(callback);
+		}
+	
 	};
-})();
 
-const gameLoadChecker = setInterval(() => {
-	if (window.quiz && window.lobby && window.viewChanger?.currentView !== undefined) {
-		clearInterval(gameLoadChecker);
-		AMQ_UTILS.__gameLoadListeners.forEach(callback => callback.apply());
-	}
-}, 300);
+	ViewChanger.prototype.changeView = (function () {
+
+		const old = ViewChanger.prototype.changeView;
+	
+		return function () {
+			
+			old.apply(this, arguments);
+	
+			AMQ_UTILS.__viewChangeListeners.forEach(callback => callback.apply());
+		};
+	})();
+
+	setTimeout(() => {
+		const gameLoadChecker = setInterval(() => {
+			if (window.quiz && window.lobby && window.viewChanger?.currentView !== undefined) {
+				clearInterval(gameLoadChecker);
+				AMQ_UTILS.__gameLoadListeners.forEach(callback => callback.apply());
+			}
+		}, 200);
+	}, 500);
+}
